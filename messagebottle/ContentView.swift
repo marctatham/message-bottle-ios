5f61398e0e6abba9e91d96447e72040824ddc934
Marc Tatham <marctatham@gmail.com>
2023-09-05 12:38:44 +0200

    initial project setup

---
+++ messagebottle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  messagebottle
+//
+//  Created by Marc Tatham on 5/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ messagebottle/messagebottleApp.swift
@@ -0,0 +1,17 @@
+//
+//  messagebottleApp.swift
+//  messagebottle
+//
+//  Created by Marc Tatham on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct messagebottleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
